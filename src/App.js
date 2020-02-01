861b3e8e4f2b9c5b9859e526bf5f1ad62771bcb9
Taeyong Kim <taeyong@deksia.com>
2020-01-31 20:56:17 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './img/logo.png';
+import './css/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img className="deksia-logo" src={logo}></img>
+        <h1>WATER LOG</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
